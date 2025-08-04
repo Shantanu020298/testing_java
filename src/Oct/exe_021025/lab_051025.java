8e00a43de8372f69ba4a10dcb2fd8957e15b4d00
shant <shantanusalokhe002@gmail.com>
2025-08-05 00:46:50 +0530

    all files

---
+++ src/Oct/exe_021025/lab_051025.java
@@ -0,0 +1,12 @@
+package Oct.exe_021025;
+
+public class lab_051025 {
+    public static void main(String[] args) {
+        String a = "abcdef";  // string is used with ""
+
+        char abc = 'A'; // used with ''
+
+        System.out.println(a);
+    }
+    }
+
