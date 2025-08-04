326232b0cfe0d504a4f50a9893c1c0a21145655d
shant <shantanusalokhe002@gmail.com>
2025-08-04 23:09:46 +0530

    all files

--- src/Oct/exe_021025/lab_031025.java
+++ src/Oct/exe_021025/lab_031025.java
@@ -5,5 +5,11 @@ import java.net.SocketOption;
 public class lab_031025 {
     public static void main(String[] args) {
         System.out.println("hii");
+
+        // this is single line comment
+
+        /* this is multi line comment
+
+         */
     }
 }
