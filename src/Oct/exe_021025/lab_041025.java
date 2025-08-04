dbbe6320b2405946baa0355dab041f072472e05f
shant <shantanusalokhe002@gmail.com>
2025-08-04 23:08:17 +0530

    all files

--- src/Oct/exe_021025/lab_041025.java
+++ src/Oct/exe_021025/lab_041025.java
@@ -1,4 +1,17 @@
 package Oct.exe_021025;
 
 public class lab_041025 {
+
+    byte age=127;
+    short age1 =12345;
+    int age2 =123456789;
+    long age3 = 1234567890L;  // small or capital L should be used at end
+
+    char $ = 'A'; //always use single quote
+
+    boolean _1 = true;
+
+    float age5 = 123.45F;   // use F or f at end
+    double $123 = 123.456;    // in case of double no need of D & d
+
 }
