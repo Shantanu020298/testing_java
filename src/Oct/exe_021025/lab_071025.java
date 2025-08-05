5b9428b998cfa95adbe424a52043821df039177c
shant <shantanusalokhe002@gmail.com>
2025-08-05 23:48:03 +0530

    all files

---
+++ src/Oct/exe_021025/lab_071025.java
@@ -0,0 +1,22 @@
+package Oct.exe_021025;
+
+public class lab_071025 {
+    public static void main(String[] args) {
+
+        final int age = 12;   // contant value cannot change by assigning after this
+        System.out.println(age);
+
+
+        // special literals
+
+        char a= '\n';  // for newline
+        char b = '\t'; // tab line
+        char c = '\b'; //one backspace of character
+        char d= '\r'; // delete previous word
+
+        System.out.println("age" + a + "new");
+        System.out.println("age" + b + "new");
+        System.out.println("age" + c + "new");
+        System.out.println("age" + d + "new");
+    }
+}
